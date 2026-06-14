208f6394872ab2be82183f74a646a7900e1bd177
kuhcokrow <kuhhhckr@gmail.com>
2026-06-14 18:48:01 +0700

    fix: configs cloudinary

--- configs/cloudinary/utils.ts
+++ configs/cloudinary/utils.ts
@@ -5,7 +5,7 @@ import { CloudinaryUploadResponse, CloudinaryTransformations } from "./types"
 // Upload image to Cloudinary
 export const uploadImage = async (
   file: Buffer | string,
-  uploadType: "avatars" | "certifications" | "documents" = "documents"
+  uploadType: "avatars" | "certifications" | "documents" | "tenants" = "documents"
 ): Promise<CloudinaryUploadResponse> => {
   const uploadOptions =
     CLOUDINARY_UPLOAD_OPTIONS[
