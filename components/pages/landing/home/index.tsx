d00012569d1a7697e275553677ddc0d3b0c781af
kuhcokrow <kuhhhckr@gmail.com>
2026-07-10 08:50:24 +0700

    fix: hidden client

--- components/pages/landing/home/index.tsx
+++ components/pages/landing/home/index.tsx
@@ -15,10 +15,10 @@ export const LandingHome = () => {
   return (
     <div className="w-full bg-linear-to-b from-slate-50 to-white">
       <Hero />
-      <Partners />
+      {/* <Partners /> */}
       <Features />
       <HowItWorks />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Pricing />
       <FAQ />
       <CTA />
